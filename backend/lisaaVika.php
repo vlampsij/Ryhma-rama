ffda2ca686f3da06bfcbe88cb2dca75223b3e613
vlampsij <veera.lampsijarvi@edu.lapinamk.fi>
2023-05-14 23:06:26 +0300

    fix

--- backend/lisaaVika.php
+++ backend/lisaaVika.php
@@ -21,3 +21,27 @@ sleep(2);
 header("location: http://localhost/vlampsij.github.io/backend/uusiVika.php");
 
 ?>
+
+<?php
+session_start();
+require "conn.php";
+
+if(isset($_POST['talleta1'])){
+        $vika = $_POST['vika'];
+        $osoite = $_POST['osoite'];
+        $tekija = $_POST['tekija'];
+
+        $komento = "INSERT INTO vikailmoitukset(vika, osoite, tekija, tyontekijaid) VALUES (:vika, :osoite, :tekija, 1)";
+        $lisaa = $conn->prepare($komento);
+        $lisaa->bindValue(':vika', $vika, PDO::PARAM_STR);
+        $lisaa->bindValue(':osoite', $osoite, PDO::PARAM_STR);
+        $lisaa->bindValue(':tekija', $tekija, PDO::PARAM_STR);
+        $lisaa->execute();
+    
+}
+
+echo "Vika lisätty";
+sleep(2);
+header("location: http://localhost/vlampsij.github.io/backend/yhteydenottopyynnot.php");
+
+?>
