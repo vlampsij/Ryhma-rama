9d182793052e16a31b0ce65b41598e6376a23817
vlampsij <veera.lampsijarvi@edu.lapinamk.fi>
2023-05-14 23:04:11 +0300

    fix

--- backend/pyynto.php
+++ backend/pyynto.php
@@ -4,12 +4,14 @@ require "conn.php";
 if(isset($_POST['laheta'])){
         $nimi = $_POST['nimi'];
         $sposti = $_POST['sposti'];
+        $osoite = $_POST['osoite'];
         $viesti = $_POST['viesti'];
 
-        $komento = "INSERT INTO yhteydenottopyynnot(pyyntoid, nimi, sposti, viesti) VALUES (NULL, :nimi, :sposti, :viesti)";
+        $komento = "INSERT INTO yhteydenottopyynnot(pyyntoid, nimi, sposti, osoite, viesti) VALUES (NULL, :nimi, :sposti, :osoite, :viesti)";
         $lisaa = $conn->prepare($komento);
         $lisaa->bindValue(':nimi', $nimi, PDO::PARAM_STR);
         $lisaa->bindValue(':sposti', $sposti, PDO::PARAM_STR);
+        $lisaa->bindValue(':osoite', $osoite, PDO::PARAM_STR);
         $lisaa->bindValue(':viesti', $viesti, PDO::PARAM_STR);
         $lisaa->execute();
 }
